d4b37947513104ce9e6f661aa153e6da6b756622
amgarrels <amgarrels@gmail.com>
2018-09-01 12:59:59 -0700

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
   resources :blogs
 
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
